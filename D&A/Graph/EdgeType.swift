02f48e008cdc39f19c831ce9d06fceb30cc106a8
Wei-Lun Su <allensu212@gmail.com>
2019-03-31 21:13:08 -0400

    implemented AdjacencyList

---
+++ D&A/Graph/EdgeType.swift
@@ -0,0 +1,14 @@
+//
+//  EdgeType.swift
+//  D&A
+//
+//  Created by Wei-Lun Su on 3/31/19.
+//  Copyright © 2019 Wei-Lun Su. All rights reserved.
+//
+
+import Foundation
+
+enum EdgeType {
+    case directed
+    case undirected
+}
